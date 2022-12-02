837650e8cfe4152893bcf2245aef1604c0d7681e
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-12-02 20:01:55 +0100

    Understanding and working with stimulus controllers, which does not work

--- app/javascript/controllers/user_modal_controller.js
+++ app/javascript/controllers/user_modal_controller.js
@@ -1,18 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="user-modal", example: "data-controller": "user-modal" for ERB.
+// Connects to data-controller="user-modal"
 export default class extends Controller {
   connect() {
-    console.log("Hello, Stimulus!");
+    console.log("Hello, Stimulus! I am connected.");
   }
 
   initialize() {
-    this.element.setAttribute("data-action", "click->user-modal#showModal");
+    this.element.setAttribute("data-action", "click->user-#showModal");
   }
 
   showModal() {
     event.preventDefault();
     this.url = this.element.getAttribute("href");
-    console.log(this.url);
+
+    fetch(this.url, {
+      headers: {
+        Accept: { "text/vnd.turbo-stream.html" },
+        "X-Requested-With": "XMLHttpRequest",
+        "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content,
+      }
+  })
+    .then((response) => response.text())
+    .then((html => Turbo.renderStreamMessage(html)))
   }
 }
