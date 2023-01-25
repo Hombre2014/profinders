ad082318f23f3c3de7b5c8e4d19934f1c4edfedd
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-01-25 14:03:39 +0100

    Connect Stimulus controller

--- app/javascript/controllers/user_modal_controller.js
+++ app/javascript/controllers/user_modal_controller.js
@@ -1,27 +1,27 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="user-modal"
 export default class extends Controller {
   connect() {
-    console.log("Hello, Stimulus! I am connected.");
+    console.log('I am connected!!!');
   }
 
   initialize() {
-    this.element.setAttribute("data-action", "click->user-#showModal");
+    this.element.setAttribute('data-action', 'click->user-modal#showModal');
   }
 
-  showModal() {
+  showModal(event) {
     event.preventDefault();
-    this.url = this.element.getAttribute("href");
+    this.url = this.element.getAttribute('href');
 
     fetch(this.url, {
       headers: {
-        Accept: { "text/vnd.turbo-stream.html" },
-        "X-Requested-With": "XMLHttpRequest",
-        "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content,
-      }
-  })
-    .then((response) => response.text())
-    .then((html => Turbo.renderStreamMessage(html)))
+        Accept: 'text/vnd.turbo-stream.html',
+        // "X-Requested-With": "XMLHttpRequest",
+        // "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content,
+      },
+    })
+      .then((response) => response.text())
+      .then((html) => Turbo.renderStreamMessage(html));
   }
 }
